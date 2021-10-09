b51d41cc5fb315e4192269b9d2bb332bf39a3412
Bubby <bubblespoupon@gmail.com>
2021-10-09 19:41:25 -0400

    forgot comments

--- Lesson1.py
+++ Lesson1.py
@@ -1,5 +1,10 @@
+#this is how to print (call)
 print("inmybutt")
+#this is how you print numbers. no quotes
 print (99)
+#this is how to create variable by assigning right to left
 carrots = ("inmybutt")
+#how to call a variable, which is defined by equals, line prior, can't print carrots w/o defining
 print (carrots)
+#defining the data type
 print (type(carrots))
